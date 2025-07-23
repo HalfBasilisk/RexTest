fd6f8f9dcdba2c71fe458f97afe40adaf79b3db3
HalfBasilisk <obtusebituse@gmail.com>
2025-07-22 23:31:53 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
 
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
 
-    burger.addEventListener('click', () => {
-        // Toggle Nav
-        nav.classList.toggle('nav-active');
-        // Toggle body scroll
-        document.body.classList.toggle('no-scroll');
-
-        // Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-            }
-        });
-
-        // Burger Animation
-        burger.classList.toggle('toggle');
-    });
-}
 
 const populateTalent = () => {
     const talentContainer = document.querySelector('.talent-container');
@@ -50,5 +27,5 @@ const populateTalent = () => {
 
 
 
-navSlide();
+
 populateTalent();
